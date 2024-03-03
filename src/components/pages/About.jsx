aa6153cace59aa5f72dd135a94e01bc5bccb53eb
gisellerx <giselle.armas@outlook.com>
2024-03-03 17:36:30 -0500

    added files

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,20 @@
+export default function About() {
+  return (
+    <section id="about">
+      <section>
+      <h1>About Me</h1>
+        <p>
+          I am Giselle Reyes. I received a degree in biology from Florida
+          International University, and graduated with Honors. During the global
+          pandemic, I made a career change. Recent graduate from the University
+          of Miami Web Development program.
+        </p>
+
+        <p>
+          Looking forward to new and exciting challenges as I progress in my
+          career.
+        </p>
+      </section>
+    </section>
+  );
+}
---
+++ src/components/pages/Blog.jsx
@@ -0,0 +1,30 @@
+export default function Blog() {
+  return (
+    <section className="section-container">
+            <section id="image1">
+                <a href="https://gisellerx.github.io/challenge1-code-refactoring/"><img src="assets/images/project1.PNG" alt="" id="hw1"/></a>
+                <h3>Horiseon</h3>
+            </section>
+
+            <section className="card">
+                <a href="https://ekovalchick.github.io/project_1/"><img src="assets/images/CosmicJamz.png" alt="" width="600px" height="400px" id="CosmicJamz"/></a>
+                <h3>CosmicJamz</h3>
+            </section>
+
+            <section className="card">
+                <a href="https://pawgress-263620beafdd.herokuapp.com/"><img src="assets/images/Pawgress.png" alt="" width="600px" height="400px" id="Pawgress"/></a>
+                <h3>Pawgress</h3>
+            </section>
+
+            <section className="card">
+                <img src="https://placehold.co/600x400" alt=""/>
+                <h3>image</h3>
+            </section>
+
+            <section className="card">
+                <img src="https://placehold.co/600x400" alt=""/>
+                <h3>image</h3>
+            </section>
+    </section>
+  );
+}
